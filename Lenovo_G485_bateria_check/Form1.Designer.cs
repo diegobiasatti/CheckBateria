1da6bf6acb312d7b0c7f6650606b2bd69dfef2d0
Diego Biasatti <biasatti.diego@gmail.com>
2022-04-28 13:59:24 -0300

    Add files via upload

---
+++ Lenovo_G485_bateria_check/Form1.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace Lenovo_G485_bateria_check
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lbl_bat_actualiza = new System.Windows.Forms.Label();
+            this.lbl_bat_status = new System.Windows.Forms.Label();
+            this.lbl_bat = new System.Windows.Forms.Label();
+            this.lbl_level_bat = new System.Windows.Forms.Label();
+            this.pic_bat = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lbl_descarga_bat = new System.Windows.Forms.Label();
+            this.lbl_carga_bat = new System.Windows.Forms.Label();
+            this.timer_bateria = new System.Windows.Forms.Timer(this.components);
+            this.timer_carga = new System.Windows.Forms.Timer(this.components);
+            this.timer_descarga = new System.Windows.Forms.Timer(this.components);
+            this.timer_des = new System.Windows.Forms.Timer(this.components);
+            this.timer_car = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer_check = new System.Windows.Forms.Timer(this.components);
+            this.timer_reporta_estado = new System.Windows.Forms.Timer(this.components);
+            this.t_uno = new System.Windows.Forms.Timer(this.components);
+            this.t_dos = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pic_bat)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbl_bat_actualiza
+            // 
+            this.lbl_bat_actualiza.AutoSize = true;
+            this.lbl_bat_actualiza.Location = new System.Drawing.Point(9, 164);
+            this.lbl_bat_actualiza.Name = "lbl_bat_actualiza";
+            this.lbl_bat_actualiza.Size = new System.Drawing.Size(22, 13);
+            this.lbl_bat_actualiza.TabIndex = 29;
+            this.lbl_bat_actualiza.Text = " 10";
+            this.lbl_bat_actualiza.Visible = false;
+            // 
+            // lbl_bat_status
+            // 
+            this.lbl_bat_status.AutoSize = true;
+            this.lbl_bat_status.Location = new System.Drawing.Point(12, 137);
+            this.lbl_bat_status.Name = "lbl_bat_status";
+            this.lbl_bat_status.Size = new System.Drawing.Size(40, 13);
+            this.lbl_bat_status.TabIndex = 28;
+            this.lbl_bat_status.Text = "Bateria";
+            this.lbl_bat_status.Visible = false;
+            // 
+            // lbl_bat
+            // 
+            this.lbl_bat.AutoSize = true;
+            this.lbl_bat.Location = new System.Drawing.Point(12, 151);
+            this.lbl_bat.Name = "lbl_bat";
+            this.lbl_bat.Size = new System.Drawing.Size(19, 13);
+            this.lbl_bat.TabIndex = 27;
+            this.lbl_bat.Text = "80";
+            this.lbl_bat.Visible = false;
+            // 
+            // lbl_level_bat
+            // 
+            this.lbl_level_bat.AutoSize = true;
+            this.lbl_level_bat.BackColor = System.Drawing.Color.White;
+            this.lbl_level_bat.Location = new System.Drawing.Point(57, 103);
+            this.lbl_level_bat.Name = "lbl_level_bat";
+            this.lbl_level_bat.Size = new System.Drawing.Size(15, 13);
+            this.lbl_level_bat.TabIndex = 26;
+            this.lbl_level_bat.Text = "%";
+            this.lbl_level_bat.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // pic_bat
+            // 
+            this.pic_bat.Image = ((System.Drawing.Image)(resources.GetObject("pic_bat.Image")));
+            this.pic_bat.Location = new System.Drawing.Point(34, 80);
+            this.pic_bat.Name = "pic_bat";
+            this.pic_bat.Size = new System.Drawing.Size(75, 140);
+            this.pic_bat.TabIndex = 25;
+            this.pic_bat.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(108, 1);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 33;
+            this.button2.Text = "descarga";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(108, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 32;
+            this.button1.Text = "carga";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lbl_descarga_bat
+            // 
+            this.lbl_descarga_bat.AutoSize = true;
+            this.lbl_descarga_bat.Location = new System.Drawing.Point(9, 19);
+            this.lbl_descarga_bat.Name = "lbl_descarga_bat";
+            this.lbl_descarga_bat.Size = new System.Drawing.Size(115, 13);
+            this.lbl_descarga_bat.TabIndex = 31;
+            this.lbl_descarga_bat.Text = "Descarga de la Bateria";
+            // 
+            // lbl_carga_bat
+            // 
+            this.lbl_carga_bat.AutoSize = true;
+            this.lbl_carga_bat.Location = new System.Drawing.Point(9, 53);
+            this.lbl_carga_bat.Name = "lbl_carga_bat";
+            this.lbl_carga_bat.Size = new System.Drawing.Size(97, 13);
+            this.lbl_carga_bat.TabIndex = 30;
+            this.lbl_carga_bat.Text = "Carga de la Bateria";
+            // 
+            // timer_bateria
+            // 
+            this.timer_bateria.Enabled = true;
+            this.timer_bateria.Interval = 5400;
+            this.timer_bateria.Tick += new System.EventHandler(this.timer_bateria_Tick);
+            // 
+            // timer_carga
+            // 
+            this.timer_carga.Enabled = true;
+            this.timer_carga.Interval = 300;
+            this.timer_carga.Tick += new System.EventHandler(this.timer_carga_Tick);
+            // 
+            // timer_descarga
+            // 
+            this.timer_descarga.Enabled = true;
+            this.timer_descarga.Tick += new System.EventHandler(this.timer_descarga_Tick);
+            // 
+            // timer_des
+            // 
+            this.timer_des.Enabled = true;
+            this.timer_des.Tick += new System.EventHandler(this.timer_des_Tick);
+            // 
+            // timer_car
+            // 
+            this.timer_car.Tick += new System.EventHandler(this.timer_car_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 13);
+            this.label1.TabIndex = 34;
+            this.label1.Text = "Descarga de la Bateria";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 13);
+            this.label2.TabIndex = 35;
+            this.label2.Text = "Carga de la Bateria";
+            // 
+            // timer_check
+            // 
+            this.timer_check.Enabled = true;
+            this.timer_check.Tick += new System.EventHandler(this.timer_check_Tick);
+            // 
+            // timer_reporta_estado
+            // 
+            this.timer_reporta_estado.Tick += new System.EventHandler(this.timer_reporta_estado_Tick);
+            // 
+            // t_uno
+            // 
+            this.t_uno.Enabled = true;
+            this.t_uno.Tick += new System.EventHandler(this.t_uno_Tick);
+            // 
+            // t_dos
+            // 
+            this.t_dos.Enabled = true;
+            this.t_dos.Tick += new System.EventHandler(this.t_dos_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(128, 224);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lbl_descarga_bat);
+            this.Controls.Add(this.lbl_carga_bat);
+            this.Controls.Add(this.lbl_bat_actualiza);
+            this.Controls.Add(this.lbl_bat_status);
+            this.Controls.Add(this.lbl_bat);
+            this.Controls.Add(this.lbl_level_bat);
+            this.Controls.Add(this.pic_bat);
+            this.Location = new System.Drawing.Point(945, 0);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Bateria MT40II";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pic_bat)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_bat_actualiza;
+        private System.Windows.Forms.Label lbl_bat_status;
+        private System.Windows.Forms.Label lbl_bat;
+        private System.Windows.Forms.Label lbl_level_bat;
+        private System.Windows.Forms.PictureBox pic_bat;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lbl_descarga_bat;
+        private System.Windows.Forms.Label lbl_carga_bat;
+        private System.Windows.Forms.Timer timer_bateria;
+        private System.Windows.Forms.Timer timer_carga;
+        private System.Windows.Forms.Timer timer_descarga;
+        private System.Windows.Forms.Timer timer_des;
+        private System.Windows.Forms.Timer timer_car;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer_check;
+        private System.Windows.Forms.Timer timer_reporta_estado;
+        private System.Windows.Forms.Timer t_uno;
+        private System.Windows.Forms.Timer t_dos;
+    }
+}
+
---
+++ Lenovo_G485_bateria_check/Form1.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Management;
+using System.Threading;
+using System.Runtime.InteropServices; //para las DllImport("user32.dll")
+using System.IO;
+
+namespace Lenovo_G485_bateria_check
+{
+    public partial class Form1 : Form
+    {
+       
+        public int lbl_bat_estado;
+        public int voltaje_bat;
+        public int voltaje;
+        public int f;
+        public int count = 0;
+        public int count1 = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+                bateria();
+                Thread.Sleep(2000);
+                if (File.Exists(@"log_ok\bat_cargando.txt")&& (File.Exists(@"log_ok\bat_descargando.txt")))
+                {
+                    File.Delete(@"log_ok\bat_cargando.txt");
+                     File.Delete(@"log_ok\bat_descargando.txt");
+
+                }
+                           
+                 
+        }
+       
+        private void check_carga_bateria()
+        {
+            voltaje_bat = Convert.ToInt32(lbl_bat.Text);
+            voltaje = Convert.ToInt32(lbl_bat_actualiza.Text);
+            //int voltaje = Convert.ToInt32(lbl_bat.Text);
+            if (voltaje > (voltaje_bat ))
+            {
+                lbl_carga_bat.Text = "Chequeando";
+            }
+            if ((voltaje  + 20) < (voltaje_bat))
+            { lbl_carga_bat.Text = "OK";
+            timer_carga.Enabled = false;
+            timer_car.Enabled = false;
+            }
+
+        }
+        private void check_descarga_bateria()
+        {
+            voltaje_bat = Convert.ToInt32(lbl_bat.Text);
+            voltaje = Convert.ToInt32(lbl_bat_actualiza.Text);
+            //int voltaje = Convert.ToInt32(lbl_bat.Text);
+            if (voltaje < (voltaje_bat))
+            {
+                lbl_descarga_bat.Text = "Chequeando";
+            }
+            if ((voltaje - 20 ) > (voltaje_bat))
+            { lbl_descarga_bat.Text = "OK";
+            //timer_descarga.Enabled = false;
+                timer_des.Enabled = false;
+            //MessageBox.Show("conecte el cargador");
+            }
+            if (lbl_bat_status.Text == "2")
+            {
+                timer_carga.Enabled = true;
+
+                check_carga_bateria();
+            }
+
+        }
+        public void bateria_voltaje()
+        {
+            try
+            {
+                ManagementObjectSearcher searcher =
+                    new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_Battery");
+
+                foreach (ManagementObject queryObj in searcher.Get())
+                {
+                    lbl_bat_actualiza.Text = queryObj["DesignVoltage"].ToString();
+                    voltaje = Convert.ToInt32(lbl_bat_actualiza.Text);
+                    //lbl_bat_actualiza.Text = voltaje.ToString();
+
+                }
+            }
+            catch (ManagementException e)
+            {
+                MessageBox.Show("An error occurred while querying for WMI data: " + e.Message);
+            }
+        }
+        public void bateria()
+        {
+            try
+            {
+                ManagementObjectSearcher searcher =
+                    new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_Battery");
+
+                foreach (ManagementObject queryObj in searcher.Get())
+                {
+                    lbl_level_bat.Text = queryObj["EstimatedChargeRemaining"].ToString();
+                    lbl_bat.Text = queryObj["DesignVoltage"].ToString();
+                    lbl_bat_status.Text = queryObj["BatteryStatus"].ToString();
+                    f = Convert.ToInt32(lbl_level_bat.Text);
+
+                    //MessageBox.Show(f.ToString());
+                    string bat_1 = (@"..\..\Resources\bat_1.png");
+                    string bat_2 = (@"..\..\Resources\bat_2.png");
+                    string bat_3 = (@"..\..\Resources\bat_3.png");
+                    string bat_4 = (@"..\..\Resources\bat_4.png");
+                    string bat_5 = (@"..\..\Resources\bat_5.png");
+                    string bat_6 = (@"..\..\Resources\bat_6.png");
+                    string bat_7 = (@"..\..\Resources\bat_7.png");
+                    string bat_8 = (@"..\..\Resources\bat_8.png");
+                    string bat_9 = (@"..\..\Resources\bat_9.png");
+                    string bat_10 = (@"..\..\Resources\bat_10.png");
+                    string bat_11 = (@"..\..\Resources\bat_11.png");
+                    string bat_12 = @"..\..\Resources\bat_12.png";
+
+                    
+                    if (f <= 5)
+                    { pic_bat.Load(bat_1);}
+                    if ((f >= 6) && (f <= 12))
+                    { pic_bat.Load(bat_2);  }
+                    if ((f >= 13) && (f <= 20))
+                    { pic_bat.Load(bat_3); }
+                    if ((f >= 21) && (f <= 30))
+                    { pic_bat.Load(bat_4); }
+                    if ((f >= 31) && (f <= 40))
+                    { pic_bat.Load(bat_5); }
+                    if ((f >= 41) && (f <= 50))
+                    { pic_bat.Load(bat_6); }
+                    if ((f >= 51) && (f <= 60))
+                    { pic_bat.Load(bat_7); }
+                    if ((f >= 61) && (f <= 67))
+                    { pic_bat.Load(bat_8); }
+                    if ((f >= 68) && (f <= 75))
+                    { pic_bat.Load(bat_9); }
+                    if ((f >= 76) && (f <= 85))
+                    { pic_bat.Load(bat_10); }
+                    if ((f >= 86) && (f <= 94))
+                    { pic_bat.Load(bat_11);  }
+                    if ((f >= 95) && (f <= 100))
+                    { pic_bat.Load(bat_12);  }
+
+                }
+            }
+            catch (ManagementException e)
+            {
+                MessageBox.Show("An error occurred while querying for WMI data: " + e.Message);
+            }
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            check_descarga_bateria();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            check_carga_bateria();
+        }
+
+        private void timer_bateria_Tick(object sender, EventArgs e)
+        {
+            bateria();
+           
+        }
+
+        private void timer_carga_Tick(object sender, EventArgs e)
+        {
+            if (lbl_bat_status.Text == "2")
+            {
+
+                bateria();
+                bateria_voltaje();
+                
+                timer_carga.Stop();
+                timer_car.Enabled = true;
+
+
+
+            }
+        }
+
+        private void timer_descarga_Tick(object sender, EventArgs e)
+        {
+            if (lbl_bat_status.Text == "1")
+            {
+
+                bateria_voltaje();
+                
+                timer_descarga.Stop();
+
+
+            }
+        }
+
+        private void timer_des_Tick(object sender, EventArgs e)
+        {
+            check_descarga_bateria();
+        }
+
+        private void timer_car_Tick(object sender, EventArgs e)
+        {
+            check_carga_bateria();
+        }
+
+        private void timer_check_Tick(object sender, EventArgs e)
+        {
+            if ((lbl_descarga_bat.Text == "OK") && (lbl_carga_bat.Text == "OK"))
+            {
+                TextWriter unidad = new StreamWriter(@"log_ok\bat_ok.txt");
+                unidad.Close();
+            }
+        }
+
+        private void timer_reporta_estado_Tick(object sender, EventArgs e)
+        {
+            if (lbl_bat_status.Text == "1")
+            {
+                
+                TextWriter descargando = new StreamWriter(@"log_ok\bat_descargando.txt");
+                descargando.Close();
+
+               
+            }
+            else
+            {
+                if (lbl_bat_status.Text == "2")
+                {
+                   
+                    TextWriter cargando = new StreamWriter(@"log_ok\bat_cargando.txt");
+                    cargando.Close();
+
+                  
+                }
+            }
+        }
+
+        private void t_uno_Tick(object sender, EventArgs e)
+        {
+            if (lbl_bat_status.Text == "1")
+            {
+                
+                TextWriter descargando = new StreamWriter(@"log_ok\bat_descargando.txt");
+                descargando.Close();
+                lbl_descarga_bat.Text = "OK";
+                t_uno.Enabled = false;
+                /*
+                     
+                timer_descarga.Enabled = true;
+                check_descarga_bateria();*/
+            }
+        }
+
+        private void t_dos_Tick(object sender, EventArgs e)
+        {
+             
+                    if (lbl_bat_status.Text == "2")
+                    {
+                      
+                        TextWriter cargando = new StreamWriter(@"log_ok\bat_cargando.txt");
+                        cargando.Close();
+                        lbl_carga_bat.Text = "OK";
+                        t_dos.Enabled = false;
+                    /*
+                        timer_carga.Enabled = true;
+                        check_descarga_bateria();
+                     */ 
+                    }
+                
+        }
+
+       
+    }
+}
